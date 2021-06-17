898163fa79802b1803de927402f3ab1f4ab3c1c7
Andrew Gutierrez <agut@comcast.net>
2021-06-16 22:24:50 -0400

    #3 First pass at new GameMangerStates

--- include/GameManager.h
+++ include/GameManager.h
@@ -9,6 +9,16 @@
 #include <geometry_msgs/PoseArray.h>
 #include <geometry_msgs/PoseStamped.h>
 #include <std_msgs/Bool.h>
+#include <std_msgs/Empty.h>
+
+enum GameState
+{
+    CALIBRATING,
+    RESTARTING,
+    IDLE,
+    AIMING,
+    SHOOTING
+};
 
 class GameManager
 {
@@ -21,7 +31,13 @@ class GameManager
         ros::NodeHandle nh_;
         ros::Subscriber has_ball_sub_;
         ros::Subscriber cup_array_sub_;
+        ros::Subscriber shot_fired_sub_;
+        ros::Subscriber calibration_request_sub_;
+        ros::Subscriber calibration_complete_sub_;
+        ros::Subscriber restart_request_sub_;
         ros::Publisher target_cup_pub_;
+        ros::Publisher detector_restart_pub_;
+        ros::Publisher detector_calibrate_pub_;
         
         // TF data
         std::string target_frame_id_;
@@ -31,12 +47,27 @@ class GameManager
         // Callbacks
         void cupArrayCallback( const geometry_msgs::PoseArray::ConstPtr& msg );
         void hasBallCallback( const std_msgs::Bool::ConstPtr& msg );
+        void shotFiredCallback( const geometry_msgs::PoseStamped::ConstPtr& msg );
+        void calibrationRequestCallback( const std_msgs::Empty::ConstPtr& msg );
+        void calibrationCompleteCallback( const std_msgs::Empty::ConstPtr& msg );
+        void restartRequestCallback( const std_msgs::Empty::ConstPtr& msg );
 
         // Msg data
         geometry_msgs::PoseArray detection_array_;
-        geometry_msgs::PoseStamped::Ptr target_cup_;
-        int target_seq_id;
+        geometry_msgs::PoseStamped target_cup_;
+        int target_seq_id_;
         bool has_ball_;
+        bool shot_success_;
+        bool calibration_request_;
+        bool calibration_complete_;
+        bool restart_request_;
+
+        // Game data
+        GameState state_; 
+        ros::Time command_sent_;
+        ros::Time calibration_sent_;
+        ros::Duration response_timeout_;
+        ros::Duration calibration_timeout_;
         
         // Utilities
         bool pickTarget();
--- src/GameManager.cpp
+++ src/GameManager.cpp
@@ -3,19 +3,29 @@
 GameManager::
 GameManager( ros::NodeHandle nh ):
     tf_listener_(tf_buffer_),
-    target_seq_id(0),
-    has_ball_(false)
+    target_seq_id_(0),
+    has_ball_(false),
+    shot_success_(false),
+    calibration_request_(false),
+    state_(GameState::IDLE) //TODO: Initialize to calibrating when setup
 {
     nh_ = nh;
-
+    
     // Pull in config data
     nh.param<std::string>("launcher_frame_id", target_frame_id_, "launcher");
+    response_timeout_ = ros::Duration(20.0);    
+    calibration_timeout_ = ros::Duration(30.0);    
 
+    // Setup Publishers and Subscribers
     cup_array_sub_ = nh_.subscribe<geometry_msgs::PoseArray>("/detector/cup_array", 1, &GameManager::cupArrayCallback, this);
     has_ball_sub_ = nh_.subscribe<std_msgs::Bool>("/launcher/has_ball", 1, &GameManager::hasBallCallback, this);
+    shot_fired_sub_ = nh_.subscribe<geometry_msgs::PoseStamped>("/launcher/shot", 1, &GameManager::shotFiredCallback, this);
+    calibration_request_sub_ = nh.subscribe<std_msgs::Empty>("/calibration/request", 1, &GameManager::calibrationRequestCallback, this);
+    calibration_complete_sub_ = nh.subscribe<std_msgs::Empty>("/calibration/complete", 1, &GameManager::calibrationCompleteCallback, this);
+    restart_request_sub_ = nh.subscribe<std_msgs::Empty>("/restart", 1, &GameManager::restartRequestCallback, this);
     target_cup_pub_ = nh_.advertise<geometry_msgs::PoseStamped>("/launcher/target_pose", 100);  
-
-    target_cup_.reset( new geometry_msgs::PoseStamped() );
+    detector_restart_pub_ = nh_.advertise<std_msgs::Empty>("/detector/restart", 100);  
+    detector_calibrate_pub_ = nh_.advertise<std_msgs::Empty>("/detector/calibrate", 100);  
 }
 
 void
@@ -32,6 +42,35 @@ hasBallCallback( const std_msgs::Bool::ConstPtr& msg )
     has_ball_ = msg->data;
 }
 
+void
+GameManager::
+shotFiredCallback( const geometry_msgs::PoseStamped::ConstPtr& msg )
+{
+    // TODO: Verify that confirmation is from the correct shot
+    shot_success_ = true;
+}
+
+void
+GameManager::
+calibrationRequestCallback( const std_msgs::Empty::ConstPtr& msg )
+{
+    calibration_request_ = true;
+}
+
+void
+GameManager::
+calibrationCompleteCallback( const std_msgs::Empty::ConstPtr& msg )
+{
+    calibration_complete_ = true;
+}
+
+void
+GameManager::
+restartRequestCallback( const std_msgs::Empty::ConstPtr& msg )
+{
+    restart_request_ = true;
+}
+
 bool
 GameManager::
 pickTarget( )
@@ -41,20 +80,19 @@ pickTarget( )
     if ( !detection_array_.poses.empty() )
     {
         // Set up target header
-        target_cup_->header.seq = target_seq_id;
-        target_cup_->header.stamp = ros::Time::now();
-        target_cup_->header.frame_id = detection_array_.header.frame_id;
-        target_seq_id++;
+        target_cup_.header.seq = target_seq_id_;
+        target_cup_.header.stamp = ros::Time::now();
+        target_cup_.header.frame_id = detection_array_.header.frame_id;
+        target_seq_id_++;
 
         // TODO: implement a real algorithm to pick the best target cup
-        target_cup_->pose = detection_array_.poses[0];
+        target_cup_.pose = detection_array_.poses[0];
 
         // Transform the the command to launcher frame
         try
         {
             // wait for the correct transform for up to 3 sec
-            geometry_msgs::PoseStamped launcher_cmd = tf_buffer_.transform(*target_cup_, target_frame_id_, ros::Duration(3.0) ); 
-            target_cup_pub_.publish(launcher_cmd);
+            target_cup_ = tf_buffer_.transform(target_cup_, target_frame_id_, ros::Duration(3.0) ); 
             target_found = true;
         }
         catch (tf2::TransformException &ex)
@@ -70,13 +108,102 @@ void
 GameManager::
 run()
 {
-    // Check for a ball, then calculate command and send to launcher
-    if ( has_ball_ )
+    switch(state_)
     {
-        if ( pickTarget() )
-        {
-            // target has been found, send launcher command      
-            has_ball_ = false;
+        case GameState::CALIBRATING:
+        { 
+            if (calibration_complete_)
+            {
+                state_ = GameState::RESTARTING;
+                ROS_INFO("Transition CALIBRATING->RESTARTING: Calibration completed sucessfully");
+            }
+            else if (ros::Time::now() - calibration_sent_ > calibration_timeout_)
+            {
+                // TODO: reload default params if possible
+                state_ = GameState::RESTARTING;
+                ROS_WARN("Transition CALIBRATING->RESTARTING: Calibration timed out, using default values");
+            }
+
+            break;
         }
+        case GameState::RESTARTING:
+        {
+            // Send reboot request to the CupDetector node
+            std_msgs::Empty restart;
+            detector_restart_pub_.publish(restart);
+
+            state_ = GameState::IDLE;
+            ROS_INFO("Transition RESTARTING->IDLE: Restart complete");
+            break;
+        }
+        case GameState::IDLE:
+        {
+            // Process calibration request
+            if (calibration_request_)
+            {
+                // Request calibration
+                std_msgs::Empty request;
+                detector_calibrate_pub_.publish(request);             
+                calibration_sent_ = ros::Time::now();
+
+                // Reset processed flags
+                calibration_request_ = false;
+                calibration_complete_ = false;
+                restart_request_ = false; // Process restart command concurrently
+                state_ = GameState::CALIBRATING;
+                ROS_INFO("Transition IDLE->CALIBRATING: Received calibration request");
+            }
+            
+            // Process restart request
+            if (restart_request_)
+            {
+                restart_request_ = false;
+                state_ = GameState::RESTARTING;
+                ROS_INFO("Transition IDLE->RESTARTING: Received restart request");
+            }
+
+            // Check for ball
+            if ( has_ball_ )
+            {
+                state_ = GameState::AIMING;
+                ROS_INFO("Transition IDLE->AIMING: Ball detected");
+            }
+
+            break;
+        }
+        case GameState::AIMING:
+        {
+            bool target_found = pickTarget();
+        
+            if ( target_found )
+            {
+                target_cup_pub_.publish(target_cup_);
+                state_ = GameState::SHOOTING;
+                command_sent_ = ros::Time::now();
+                shot_success_ = false;
+                ROS_INFO("Transition AIMING->SHOOTING: Target found");
+            }
+            else
+            {
+                ROS_WARN("Transition AIMING->IDLE: Target NOT found");
+                state_ = GameState::IDLE;
+            }
+
+            break;
+        }
+        case GameState::SHOOTING:
+        {
+            if (shot_success_)
+            {
+                ROS_INFO("Transition SHOOTING->IDLE: Received shot confimation from launcher");
+                state_ = GameState::IDLE;  
+            }
+            else if  ( ros::Time::now() - command_sent_ > response_timeout_ )
+            {
+                ROS_WARN("Transition SHOOTING->IDLE: Timed out waiting for launcher response");
+                state_ = GameState::IDLE;  
+            }
+            break;
+        } 
     }
 }
