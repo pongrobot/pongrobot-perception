e540446e2e6308dbb175ea30307dd80fe069ac9e
Andrew Gutierrez <agut@comcast.net>
2021-07-07 09:30:11 -0400

    Add scripts to install, start, stop, and uninstall system service for launch file

---
+++ launch/scripts/stop_service.sh
@@ -0,0 +1,5 @@
+# Stop the pong robot system service
+# See http://docs.ros.org/en/jade/api/robot_upstart/html/install.html
+
+echo "Stopping pong robot system service..."
+sudo service pongrobot stop
---
+++ launch/scripts/uninstall_service.sh
@@ -0,0 +1,5 @@
+# Uninstall the pong robot system service
+# See http://docs.ros.org/en/jade/api/robot_upstart/html/install.html
+
+echo "Uninstalling pong robot system service..."
+rosrun robot_upstart uninstall pongrobot
